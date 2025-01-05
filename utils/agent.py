6003228b0559a80a27746e42674ec1b1f6c694d2
Jayreddin <jayreddin@hotmail.com>
2025-01-05 12:10:12 +0000

    11

--- utils/agent.py
+++ utils/agent.py
@@ -13,7 +13,7 @@ def create_clevrr_agent(model, prompt):
     df_locals = {}
     df_locals["pg"] = pg
     tools = [PythonAstREPLTool(locals=df_locals), get_screen_info]
-    model = MODELS["openai"]
+    model = MODELS["gemini"]
     agent = create_react_agent(model, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True, return_intermediate_steps=True)
     return agent_executor
